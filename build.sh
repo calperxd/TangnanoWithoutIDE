23b3f27ea7877005114834db134972c328f50961
calper <calper@pop-os.localdomain>
2024-02-21 19:34:38 -0400

    build: now the user only buildenv, build, flash and clean options to use to become the script ease to use

--- build.sh
+++ build.sh
@@ -21,73 +21,50 @@ BOARD="board/tangnano9k.cst"
 BITSTREAM="out/bitstream.fs"
 TOP_ENTITY="top" # Edit this to change the top entity
 
-# If the first argument is "buildenv", clone the yosys repository and build the Docker image
-if [ "$1" == "buildenv" ]; then
-    docker build -t verilog_project:latest -f Docker/Dockerfile .
-    docker run -it --privileged -v $(pwd):/app -v /dev:/dev verilog_project:latest
-    exit 0
-fi
-
-# Create necessary directories
-mkdir -p out
-mkdir -p rtl
-mkdir -p synth
-
-# If the first argument is "clean", clean up the directories
-if [ "$1" == "clean" ]; then
-    echo "Cleaning up..."
-    rm -rf rtl/
-    rm -rf synth/
-    rm -rf out/
-    exit 0  # Exit after cleaning
-fi
-
-# If the first argument is "flash", flash the bitstream to the FPGA
-if [ "$1" == "flash" ]; then
-    # Check for .fs file in out/ directory
-    if [ ! -f out/*.fs ]; then
-        echo "Error: No .fs file found in out/ directory!"
+case "$1" in
+    buildenv)
+        docker build -t verilog_project:latest -f Docker/Dockerfile .
+        docker run -it --privileged -v "$(pwd)":/app -v /dev:/dev verilog_project:latest /bin/bash -c "./setenv.sh; bash"
+        exit 0
+        ;;
+    build)
+        mkdir -p out rtl synth
+        VERILOG_FILES=$(find src/ -type f -name "*.v" | tr '\n' ' ')
+        YOSYS_CMD="yosys -p \"read_verilog $VERILOG_FILES; hierarchy -check; opt; synth_gowin -top $TOP_ENTITY -json $INPUT_SYNTH_JSON; write_json $RTL_JSON;\""
+        eval $YOSYS_CMD
+        if [ $? -ne 0 ]; then
+            echo "yosys command failed!"
+            exit 1
+        fi
+        nextpnr-gowin --quiet --json $INPUT_SYNTH_JSON --write $OUTPUT_SYNTH_JSON --device $DEVICE --family $FAMILY --cst $BOARD
+        if [ $? -ne 0 ]; then
+            echo "nextpnr-gowin command failed!"
+            exit 1
+        fi
+        gowin_pack -d $FAMILY -o $BITSTREAM $OUTPUT_SYNTH_JSON
+        
+        echo ""
+        echo ""
+        echo ""
+        echo "Verilog files: $VERILOG_FILES"
+        echo "Top entity: $TOP_ENTITY"
+        echo "Bitstream generated : $BITSTREAM"
+        ;;
+    clean)
+        echo "Cleaning up..."
+        rm -rf rtl/ synth/ out/
+        ;;
+    flash)
+        if [ ! -f out/*.fs ]; then
+            echo "Error: No .fs file found in out/ directory!"
+            exit 1
+        fi
+        echo "Flashing..."
+        openFPGALoader -f -b tangnano9k $BITSTREAM
+        echo "Flashed"
+        ;;
+    *)
+        echo "Error: Invalid option. Valid options are buildenv, build, clean, flash."
         exit 1
-    fi
-    
-    echo "Flashing..."
-    openFPGALoader -f -b tangnano9k $BITSTREAM
-    echo "Flashed"
-    exit 0  # Exit after cleaning
-fi
-
-
-# Function to find all Verilog files
-find_verilog_files() {
-    find src/ -type f -name "*.v" | tr '\n' ' '
-}
-
-# Construct the yosys command
-VERILOG_FILES=$(find_verilog_files)
-YOSYS_CMD="yosys -p \"read_verilog $VERILOG_FILES; hierarchy -check; opt; synth_gowin -top $TOP_ENTITY -json $INPUT_SYNTH_JSON; write_json $RTL_JSON;\""
-
-# Run yosys
-eval $YOSYS_CMD
-if [ $? -ne 0 ]; then
-    echo "yosys command failed!"
-    exit 1
-fi
-
-# Execute place and route with nextpnr-gowin
-nextpnr-gowin   --quiet \
-                --json $INPUT_SYNTH_JSON \
-                --write $OUTPUT_SYNTH_JSON \
-                --device $DEVICE \
-                --family $FAMILY \
-                --cst $BOARD
-if [ $? -ne 0 ]; then
-    echo "nextpnr-gowin command failed!"
-    exit 1
-fi
-
-gowin_pack -d $FAMILY -o $BITSTREAM $OUTPUT_SYNTH_JSON
-
-# Display the used Verilog files and top entity
-echo "Verilog files: $VERILOG_FILES"
-echo "Top entity: $TOP_ENTITY"
-echo "Bitstream generated : $BITSTREAM"
+        ;;
+esac
